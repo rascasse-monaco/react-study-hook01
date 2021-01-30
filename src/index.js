d34eea094818c43f72ad08c20275778919354dc3
rascasse-monaco <mktwkhr@gmail.com>
2021-01-30 15:14:58 +0900

    Classを使用した書き方もサンプルで加える

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Example = () => {
+// hookで記述
+const ExampleHook = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>You clicked {count} times (Use hook)</p>
       <button onClick={() => setCount(count +1)}>Click Me</button>
+    </div>
+  );
+}
+// 上記と同様の処理をClassを使って書いたもの
+class ExampleClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  render() {
+    return (
+      <div>
+      <p>You clicked {this.state.count} times (Use Class)</p>
+      <button
+        onClick={() => this.setState({ count: this.state.count + 1 })}>
+        Click Me
+      </button>
+    </div>
+    );
+  }
+}
+// 下のようにする記述してそれをレンダーするとReactDOM.renderを沢山書かなくていい。
+const Render = () => {
+  return (
+    <div>
+      <ExampleHook />
+      <ExampleClass />
     </div>
   );
 }
 
 ReactDOM.render (
-  <Example />,
-  document.getElementById('root')
+  <Render />,
+  document.getElementById('example2')
 );
